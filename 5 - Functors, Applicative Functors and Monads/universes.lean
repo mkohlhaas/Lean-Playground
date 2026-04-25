639c31757a13d595558f63972be132f9ed3aafce
Hans-J. Schmid <knock@myopendoor.de>
2026-04-25 18:21:32 +0200

    universes

--- 5 - Functors, Applicative Functors and Monads/universes.lean
+++ 5 - Functors, Applicative Functors and Monads/universes.lean
@@ -3,16 +3,32 @@
 /- ========= -/
 
 -- A universe is a type that classifies other types.
--- Two of them are familiar: Type and Prop.
--- Type classifies ordinary types                        , such as Nat, String, Int → String × Char, IO Unit, …
--- Prop classifies propositions that may be true or false, such as "nisse" = "elf", 3 > 2, …
+-- Two familiar universes are Type and Prop.
 
 -- The whole point of the universe system is to rule out self-referential types. 
+-- Avoids GIRARD'S PARADOX and RUSSELL'S PARADOX.
 
--- the type of Prop is Type
+-- Type (= `Type 0`) classifies ordinary types, such as Nat, String, Int → String × Char, IO Unit, …
+-- Nat's    type is Type
+-- String's type is Type
+-- …
+-- Prop's   type is Type
+#check Nat                                      
+#check String                                   
+#check Int → String × Char                      
+#check IO Unit                                  
 #check Prop                                     
 
-#check Type                                     
+-- Prop classifies propositions that may be true or false, such as "nisse" = "elf", 3 > 2, …
+#check "nisse" = "elf"                          
+#check 3 > 2                                    
+
+-- the type of `Type`   is `Type 1`
+-- the type of `Type 1` is `Type 2`
+-- the type of `Type 2` is `Type 3`
+-- the type of `Type 3` is `Type 4`
+-- …
+#check Type 0                                   
 #check Type 1                                   
 #check Type 2                                   
 #check Type 3                                   
@@ -20,13 +36,18 @@
 
 -- Function types occupy the smallest universe that can contain both the argument type and the return type. 
 #check Nat → Nat                                
-#check Type → Type                              
+#check Type 0 → Type 0                          
 #check Type 1 → Type 2                          
+#check Type 1 → Type 2                          
+#check Type 2 → Type 1                          
+#check Type 5 → Type 3                          
+#check Type 3 → Type 5                          
+-- …
 
--- There is one exception to this rule.
+-- NOTE: There is one exception to this rule!
 -- If the return type of a function is a Prop, then the whole function type is in Prop.
 #check (n : Nat) → n = n + 0                    
-#check Type → 2 + 2 = 4                         
+#check Type      → 2 + 2 = 4                    
 
 /- ------------------ -/
 /- User Defined Types -/
@@ -37,26 +58,30 @@ inductive MyList (α : Type) : Type where
   | cons : α → MyList α → MyList α
 
 -- MyList is a Type → Type
-#check MyList
+#print MyList                                   
 
--- it cannot be used to contain actual types
+-- it cannot be used to contain types
 def myListOfNat : MyList Type := .cons Nat .nil 
 
--- the argument to cons with type α is from a larger universe than MyList
-inductive MyList' (α : Type 1) : Type where
-  | nil  : MyList' α
-  | cons : α → MyList' α → MyList' α            
-
+-- NOTE: 
 -- The specific rules that govern whether a datatype is allowed are somewhat complicated.
 -- Generally speaking, it's easiest to start with the datatype in the same universe as the largest of its arguments.
 -- Then, if Lean rejects the definition, increase its level by one, which will usually go through.
+
+-- the argument to cons with type α is from a larger universe than MyList
+-- NOTE: Play with Type 0, Type 1, …; comment out erroneous lines if necessary!
+inductive MyList' (α : Type 1) : Type 0 where
+  | nil  : MyList' α
+  /- | cons : α → MyList' α → MyList' α             -/
+
+#print MyList'                                  
 
 /- --------------------- -/
 /- Universe Polymorphism -/
 /- --------------------- -/
 
--- Type arguments are conventionally named with Greek letters.
--- Universe arguments are conventionally named u, v, and w.
+-- TYPE ARGUMENTS     are conventionally named with Greek letters.
+-- UNIVERSE ARGUMENTS are conventionally named u, v, and w.
 
 inductive MyList'' (α : Type u) : Type u where
   | nil  : MyList'' α
@@ -66,38 +91,55 @@ def myListOfNumbers : MyList'' Nat           := .cons 0 (.cons 1 .nil)
 def myListOfNat'    : MyList'' Type          := .cons Nat .nil
 def myListOfList    : MyList'' (Type → Type) := .cons MyList'' .nil
 
--- level arguments are written with a dot and curly braces
-#check MyList.{0}                               
+#check myListOfNumbers
+#check myListOfNat'   
+#check myListOfList   
 
+-- each occurrence of MyList'' is provided with a universe level argument
+-- level arguments are written with a dot and curly braces
 def myListOfNumbers' : MyList''.{0} Nat           := .cons 0 (.cons 1 .nil)
 def myListOfNat''    : MyList''.{1} Type          := .cons Nat .nil
 def myListOfList'    : MyList''.{1} (Type → Type) := .cons MyList''.{0} .nil
 
+-- when a universe-polymorphic definition takes multiple types as arguments,
+-- it's a good idea to give each argument its own level variable for maximum
+-- flexibility
 inductive MySum (α : Type u) (β : Type u) : Type u where
   | inl : α → MySum α β
   | inr : β → MySum α β
 
+-- can be used at multiple levels
 def stringOrNat  : MySum String Nat  := .inl "hello"
 def typeOrType   : MySum Type Type   := .inr Nat
--- both arguments must be in the same universe
+
+-- but both arguments must be in the same universe (there's only one universe argument `u` in the definition of MySum)
 def stringOrType : MySum String Type := .inr Nat
 
+-- more flexible by using different variables the two type universe levels, and
+-- then declaring that the resulting datatype is in the largest of the two
 inductive MySum' (α : Type u) (β : Type v) : Type (max u v) where
   | inl : α → MySum' α β
   | inr : β → MySum' α β
 
-def stringOrType' : MySum' String Type := .inr Nat -- arguments from different universes possible
-
+-- arguments from different universes possible
+def stringOrType' : MySum' String Type := .inr Nat
 
 -- In positions where Lean expects a universe level, any of the following are allowed:
--- · a concrete level, like 0 or 1
--- · a variable that stands for a level, such as u or v
--- · the maximum of two levels, written as max applied to the levels
--- · a level increase, written with + 1
+-- · a concrete level, like 0, 1, 2, …
+-- · a variable that stands for a level, like `u` or `v`
+-- · the maximum of two levels, written as `max` applied to the levels, like `max u v`
+-- · a level increase, written with + 1, like `(max u v) + 1`
 
 /- ---------------------------------------- -/
 /- Writing Universe-Polymorphic Definitions -/
 /- ---------------------------------------- -/
+
+-- Guidelines:
+-- · independent type arguments should have different universe variables
+-- · the whole type is itself typically either in the maximum of all the universe variables `(max u v)` or
+-- · one greater than this maximum `(max u v) + 1`
+--   · try the smaller first
+-- · Non-polymorphic types, such as Nat and String, can be placed directly in Type 0 (= Type)
 
 /- --------------------- -/
 /- Prop and Polymorphism -/
@@ -113,12 +155,14 @@ def someTruePropositions : List Prop := [
 ]
 
 -- Prop is a Type.
--- Behind the scenes, Prop and Type are united into a single hierarchy called Sort.
-def someTruePropositions' : List.{0} Prop := [
+def someTruePropositions' : List.{0} Prop := [ -- with universe argument (0) to proove the point
   1 + 1 = 2,
   "Hello, " ++ "world!" = "Hello, world!"
 ]
 
-/- ------------------------ -/
-/- Polymorphism in Practice -/
-/- ------------------------ -/
+-- Prop and Type are united into a single hierarchy called Sort.
+
+-- Prop is the same as Sort 0, Type 0 is Sort 1, Type 1 is Sort 2, …
+-- Type u is the same as Sort (u+1). When writing programs with Lean, this is
+-- typically not relevant, but it may occur in error messages from time to
+-- time, and it explains the name of the CoeSort class.
